cb6de090838147cc15f7ec4c24feea5ecbcf61e5
mattenbar <matthw.enbar@gmail.com>
2020-08-06 17:49:54 -0400

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component{
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: this.props.value
+    } // ...define initial state with a key of 'color' set to the 'value' prop
+  }
+
+  handleClick = () => {
+    //Create a click listener which, when activated, updates the state to the following hex value: '#333'
+    this.setState({
+      color: '#333'
+    })
+  }
+
+  render (){
+    return(
+      <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.handleClick}></div>
+    )
+  }
+}
